593af999323526803819cbd674598e1d7eca484d
Hsu Still <341464@gmail.com>
2017-07-27 03:28:25 +0800

    Renamed `RequiredOwner` to `RequiredOwners`

--- GeneralBot/Preconditions/RequireOwners.cs
+++ GeneralBot/Preconditions/RequireOwners.cs
@@ -12,7 +12,7 @@ namespace GeneralBot.Preconditions
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
             var config = services.GetService<ConfigModel>();
-            return config.Owners.Contains(context.User.Id) ? Task.FromResult(PreconditionResult.FromSuccess()) : Task.FromResult(PreconditionResult.FromError($"Command can only be run by the bot owners."));
+            return config.Owners.Contains(context.User.Id) ? Task.FromResult(PreconditionResult.FromSuccess()) : Task.FromResult(PreconditionResult.FromError("Command can only be run by the bot owners."));
         }
     }
 }
