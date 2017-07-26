a4fcf2acff57ef28d5235ed0473e1b3276ab218b
Hsu Still <341464@gmail.com>
2017-07-26 23:12:40 +0800

    Added descriptions for server commands

--- GeneralBot/Commands/Admin/ConfigModule.cs
+++ GeneralBot/Commands/Admin/ConfigModule.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using GeneralBot.Databases.Context;
 using GeneralBot.Results;
@@ -10,12 +8,16 @@ using GeneralBot.Results;
 namespace GeneralBot.Commands.Admin
 {
     [Group("server")]
+    [Summary("Server-specific Settings")]
+    [Remarks("Server settings for admins.")]
     [RequireContext(ContextType.Guild)]
+    [RequireUserPermission(GuildPermission.Administrator)]
     public class ConfigModule : ModuleBase<SocketCommandContext>
     {
         public CoreContext CoreSettings { get; set; }
 
         [Command("prefix")]
+        [Summary("Changes the command prefix.")]
         public async Task<RuntimeResult> ConfigPrefix(string prefix)
         {
             var dbEntry = CoreSettings.GuildsSettings.SingleOrDefault(x => x.GuildId == Context.Guild.Id) ?? new GuildSettings();
