fdd12d8f5f1ee0d6675d3c319c0f85eb294a4555
Hsu Still <341464@gmail.com>
2017-07-27 04:41:11 +0800

    Added configurable 8ball iamge

--- GeneralBot/Commands/User/FunModule.cs
+++ GeneralBot/Commands/User/FunModule.cs
@@ -19,12 +19,19 @@ namespace GeneralBot.Commands.User
         [Summary("Ask it any questions!")]
         public async Task<RuntimeResult> EightBall([Remainder] string input)
         {
-            int responseCount = Config.Commands.EightBallResponse.Count;
+            int responseCount = Config.Commands.EightBall.Responses.Count;
             if (responseCount == 0)
                 // This should hopefully never happen.
                 return CommandRuntimeResult.FromError("The 8ball responses are not yet set, contact the bot developers.");
-            string response = Config.Commands.EightBallResponse[Random.Next(0, responseCount)];
-            var embed = new EmbedBuilder {Color = InputHelper.GetRandomColor(), Title = "The Magic 8Ball"}.AddField("You asked...", input).AddField("The 8 ball says...", response);
+            string response = Config.Commands.EightBall.Responses[Random.Next(0, responseCount)];
+            var embed = new EmbedBuilder
+            {
+                Color = InputHelper.GetRandomColor(),
+                Title = "The Magic 8Ball",
+                ThumbnailUrl = Config.Commands.EightBall.Image
+            }
+            .AddField("You asked...", input)
+            .AddField("The 8 ball says...", response);
             await ReplyAsync("", embed: embed);
             return CommandRuntimeResult.FromSuccess();
         }
--- GeneralBot/Models/ConfigModel.cs
+++ GeneralBot/Models/ConfigModel.cs
@@ -11,24 +11,25 @@ namespace GeneralBot.Models
 
     public class Command
     {
-        public List<string> EightBallResponse { get; set; } = new List<string>
-        {
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt!",
-            "Yes, definitely!",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful."
-        };
+        public (List<string> Responses, string Image) EightBall { get; set; } = (new List<string>
+            {
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt!",
+                "Yes, definitely!",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+            },
+            "https://i.imgur.com/eAAerQJ.png");
     }
 
     public class Credential
