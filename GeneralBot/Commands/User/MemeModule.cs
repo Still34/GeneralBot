08d19266e9d249e79ff79980744258c853c0b4f5
Hsu Still <341464@gmail.com>
2017-07-28 00:06:20 +0800

    why not

---
+++ GeneralBot/Commands/User/MemeModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using GeneralBot.Results;
+
+namespace GeneralBot.Commands.User
+{
+    public class MemeModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("expand")]
+        public Task<RuntimeResult> ExpandMeme([Remainder] string input)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                sb.Append(c);
+                sb.Append(" ");
+            }
+            return Task.FromResult<RuntimeResult>(CommandRuntimeResult.FromInfo(sb.ToString()));
+        }
+    }
+}
