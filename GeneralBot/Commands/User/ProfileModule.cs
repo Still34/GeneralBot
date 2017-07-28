af6f60047760b71770a629737432b664bd24ec6a
Hsu Still <341464@gmail.com>
2017-07-29 05:18:16 +0800

    Fixed broken DI attempt

--- GeneralBot/Commands/User/ProfileModule.cs
+++ GeneralBot/Commands/User/ProfileModule.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
+using GeneralBot.Models;
 using GeneralBot.Models.Context;
 using GeneralBot.Results;
-using GeneralBot.Services;
-using GeneralBot.Templates;
-using GeneralBot.Models;
 
 namespace GeneralBot.Commands.User
 {
     [Group("profile")]
     public class ProfileModule : ModuleBase<SocketCommandContext>
     {
-        public readonly UserContext UserSettings;
-        public readonly ConfigModel Config;
+        public UserContext UserSettings { get; set; }
+        public ConfigModel Config { get; set; }
 
         [Command("balance")]
         [Summary("Shows your current balance.")]
-        public async Task<RuntimeResult> Balance()
+        public Task<RuntimeResult> Balance()
         {
-            var dbEntry = UserSettings.Profile.SingleOrDefault(x => x.UserId == Context.User.Id) ?? UserSettings.Profile.Add(new Profile { UserId = Context.User.Id, LastMessage = Context.Message.Timestamp }).Entity;
-            return CommandRuntimeResult.FromSuccess($"Your current balance is {dbEntry.Balance}{Config.CurrencySymbol}");
+            var dbEntry = UserSettings.Profile.SingleOrDefault(x => x.UserId == Context.User.Id) ?? UserSettings.Profile.Add(new Profile {UserId = Context.User.Id, LastMessage = Context.Message.Timestamp}).Entity;
+            return Task.FromResult<RuntimeResult>(CommandRuntimeResult.FromSuccess($"Your current balance is {dbEntry.Balance}{Config.CurrencySymbol}"));
         }
     }
 }
