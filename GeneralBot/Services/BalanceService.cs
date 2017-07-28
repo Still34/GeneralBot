a39b821ee16afc09febc93ac3530b953201a47cb
Hsu Still <341464@gmail.com>
2017-07-29 05:35:02 +0800

    Removed redundant null check; added conditioning

--- GeneralBot/Services/BalanceService.cs
+++ GeneralBot/Services/BalanceService.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
-using GeneralBot.Databases.Context;
-using GeneralBot.Extensions;
 using GeneralBot.Models.Context;
 
 namespace GeneralBot.Services
 {
     internal class BalanceService
     {
-        private readonly UserContext _userSettings;
         private readonly LoggingService _loggingService;
+        private readonly UserContext _userSettings;
 
         public BalanceService(DiscordSocketClient client, UserContext usersettings, LoggingService loggingService)
         {
@@ -22,23 +19,22 @@ namespace GeneralBot.Services
             _loggingService = loggingService;
         }
 
-        private async Task AwardBalance(SocketMessage msg)
+        private async Task AwardBalance(SocketMessage msgArg)
         {
-            var user = msg.Author;
-            var dbEntry = _userSettings.Profile.SingleOrDefault(x => x.UserId == user.Id) ?? _userSettings.Profile.Add(new Profile { UserId = user.Id, LastMessage = msg.Timestamp }).Entity;
-            var balanceIncrement = Convert.ToUInt32(new Random().Next(1, 10));
-            if(dbEntry.LastMessage == null) // Does this ever happen?
+            if (msgArg is SocketUserMessage msg && msg.Channel is SocketGuildChannel)
             {
-                dbEntry.LastMessage = msg.Timestamp;
-                dbEntry.Balance = dbEntry.Balance + balanceIncrement;
+                var user = msg.Author;
+                var dbEntry = _userSettings.Profile.SingleOrDefault(x => x.UserId == user.Id) ?? _userSettings.Profile.Add(new Profile {UserId = user.Id, LastMessage = msg.Timestamp}).Entity;
+                uint balanceIncrement = Convert.ToUInt32(new Random().Next(1, 10));
+                if (msg.Timestamp >= dbEntry.LastMessage.AddMinutes(1))
+                {
+                    await _loggingService.Log($"Increasing {user}'s balance by {balanceIncrement}...", LogSeverity.Debug).ConfigureAwait(false);
+                    dbEntry.LastMessage = msg.Timestamp;
+                    dbEntry.Balance = dbEntry.Balance + balanceIncrement;
+                }
+                _userSettings.Update(dbEntry);
+                await _userSettings.SaveChangesAsync();
             }
-            else if(msg.Timestamp >= dbEntry.LastMessage.AddMinutes(1))
-            {
-                dbEntry.LastMessage = msg.Timestamp;
-                dbEntry.Balance = dbEntry.Balance + balanceIncrement;
-            }
-            _userSettings.Update(dbEntry);
-            await _userSettings.SaveChangesAsync();
         }
     }
 }
