694a41554e88cbbca43bf573ba14582c0ee95883
Hsu Still <341464@gmail.com>
2017-07-30 16:21:37 +0800

    Added color cache

--- GeneralBot/Extensions/Helpers/ColorHelper.cs
+++ GeneralBot/Extensions/Helpers/ColorHelper.cs
@@ -6,13 +6,24 @@ namespace GeneralBot.Extensions.Helpers
 {
     public class ColorHelper
     {
+        private List<Color> _colorCache = new List<Color>;
+
         public static Color GetRandomColor()
         {
-            var random = new Random();
-            var fields = typeof(Color).GetFields();
-            var color = (Color) fields[random.Next(0, fields.Length)].GetValue(null);
-            return color;
+            if (_colorCache.Length == 0)
+            {
+                var random = new Random();
+                var fields = typeof(Color).GetFields();
+                foreach (var field in fields)
+                {
+                    var color = field.GetValue(null);
+                    if (color is Color)
+                        _colorCache.Add(color);
+                }
+            }
+            return _colorCache[random.Next(0, _colorCache.Length)];
         }
+
 
         public static float GetLuminanceFromColor(byte r, byte g, byte b) => 0.2126f * r + 0.7152f * g + 0.0722f * b;
     }
