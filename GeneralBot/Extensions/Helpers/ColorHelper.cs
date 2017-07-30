d33e68afa380b01a71e4f38997e2c829687ef358
Andrew <andrewmotion2@gmail.com>
2017-07-30 13:32:27 +0300

    fix still's code

--- GeneralBot/Extensions/Helpers/ColorHelper.cs
+++ GeneralBot/Extensions/Helpers/ColorHelper.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Reflection;
 using Discord;
+using System.Collections.Generic;
 
 namespace GeneralBot.Extensions.Helpers
 {
     public class ColorHelper
     {
-        private List<Color> _colorCache = new List<Color>;
+        private static List<Color> _colorCache = new List<Color>();
 
         public static Color GetRandomColor()
         {
-            if (_colorCache.Length == 0)
+            var random = new Random();
+            if (_colorCache.Count == 0)
             {
-                var random = new Random();
                 var fields = typeof(Color).GetFields();
                 foreach (var field in fields)
                 {
                     var color = field.GetValue(null);
                     if (color is Color)
-                        _colorCache.Add(color);
+                        _colorCache.Add((Color)color);
                 }
             }
-            return _colorCache[random.Next(0, _colorCache.Length)];
+            return _colorCache[random.Next(0, _colorCache.Count)];
         }
-
 
         public static float GetLuminanceFromColor(byte r, byte g, byte b) => 0.2126f * r + 0.7152f * g + 0.0722f * b;
     }
