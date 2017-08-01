54c979fa4cea33fb5671515881694c8694708a7d
Andrew <andrewmotion2@gmail.com>
2017-08-01 16:45:08 +0300

    really basic audio methods

--- GeneralBot/Common/Types/Queue.cs
+++ GeneralBot/Common/Types/Queue.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace GeneralBot.Common.Types
 {
@@ -12,7 +13,17 @@ namespace GeneralBot.Common.Types
             Guild = guild;
         }
 
-        public ulong Guild {get; set;}
-        public List<Song> Songs {get; set;}
+        public ulong Guild { get; set; }
+        public List<Song> Songs { get; set; }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+        }
+        
+        public void RemoveSong(Song song)
+        {
+            Songs.Remove(song);
+        }
     }
 }
--- GeneralBot/Services/Audio/AudioService.cs
+++ GeneralBot/Services/Audio/AudioService.cs
@@ -18,5 +18,40 @@ public class AudioService
         _logger = logger;
     }
 
+    public async Task JoinChannel(IGuild guild, IVoiceChannel target)
+    {
+        IAudioClient client;
+        if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            return;
+        if (target.Guild.Id != guild.Id)
+            return;
 
+        var audioClient = await target.ConnectAsync();
+
+        if (ConnectedChannels.TryAdd(guild.Id, audioClient))
+        {
+            await _logger.Log($"Connected to {guild.Name}({guild.Id})", LogSeverity.Info);
+        }
+    }
+
+    public async Task LeaveGuild(IGuild guild)
+    {
+        IAudioClient client;
+        if (ConnectedChannels.TryRemove(guild.Id, out client))
+        {
+            await client.StopAsync();
+            await _logger.Log($"Disconnected from {guild.Name}({guild.Id})", LogSeverity.Info);
+        }
+    }
+
+    private Process CreateStream(string streamUrl)
+    {
+        return Process.Start(new ProcessStartInfo
+        {
+            FileName = "ffmpeg.exe",
+            Arguments = $"-hide_banner -loglevel panic -i {streamUrl} -ac 2 -f s16le -ar 48000 pipe:1",
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        });
+    }
 }
