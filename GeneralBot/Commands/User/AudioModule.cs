ee42b07c7d3aeafb621154b60ff358c2384d147e
Hsu Still <341464@gmail.com>
2017-08-02 20:47:21 +0800

    get this constructor injection shit outta here

--- GeneralBot/Commands/User/AudioModule.cs
+++ GeneralBot/Commands/User/AudioModule.cs
@@ -4,8 +4,6 @@ namespace GeneralBot.Commands.User
 {
     public class AudioModule : ModuleBase<SocketCommandContext>
     {
-        private readonly AudioService _service;
-
-        public AudioModule(AudioService service) => _service = service;
+        public AudioService AudioService { get; set; }
     }
 }
