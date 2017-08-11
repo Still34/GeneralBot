e7e71887e970014bec87bff847e98ebd542d6e6c
Hsu Still <341464@gmail.com>
2017-08-12 06:18:30 +0800

    You know we have a Random injected right

--- GeneralBot/Services/BalanceService.cs
+++ GeneralBot/Services/BalanceService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -10,13 +9,16 @@ namespace GeneralBot.Services
     public class BalanceService
     {
         private readonly LoggingService _loggingService;
+        private readonly Random _random;
         private readonly IUserRepository _userSettings;
 
-        public BalanceService(DiscordSocketClient client, IUserRepository usersettings, LoggingService loggingService)
+        public BalanceService(DiscordSocketClient client, IUserRepository usersettings, LoggingService loggingService,
+            Random random)
         {
             client.MessageReceived += AwardBalanceAsync;
             _userSettings = usersettings;
             _loggingService = loggingService;
+            _random = random;
         }
 
         private async Task AwardBalanceAsync(SocketMessage msgArg)
@@ -27,15 +29,15 @@ namespace GeneralBot.Services
             {
                 var user = msg.Author;
                 var dbEntry = await _userSettings.GetOrCreateProfileAsync(user);
-                uint balanceIncrement = Convert.ToUInt32(new Random().Next(1, 10));
+                uint balanceIncrement = Convert.ToUInt32(_random.Next(1, 10));
                 if (msg.Timestamp >= dbEntry.LastMessage.AddMinutes(1))
                 {
                     await _loggingService.LogAsync($"Increasing {user}'s balance by {balanceIncrement}...",
                         LogSeverity.Debug).ConfigureAwait(false);
                     dbEntry.LastMessage = msg.Timestamp;
                     dbEntry.Balance = dbEntry.Balance + balanceIncrement;
+                    await _userSettings.SaveRepositoryAsync();
                 }
-                await _userSettings.SaveRepositoryAsync();
             }
         }
     }
